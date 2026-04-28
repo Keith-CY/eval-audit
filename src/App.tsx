74212b2d69e4079764786d6b0772b4e2094eace0
Keith <keithwhisper@gmail.com>
2026-04-29 04:55:20 +0900

    chore: scaffold evaluation review app

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="app-shell">
+      <h1>Evaluation Review</h1>
+      <p>Upload an evaluation zip to begin.</p>
+    </main>
+  );
+}
